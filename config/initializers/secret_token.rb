280f9d689f043640250a4b3016f3c3d8bba561fa
deniseymoran <deniseymoran@gmail.com>
2016-08-11 22:43:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mathsite::Application.config.secret_key_base = '12a5c06c3cd49ce8bcc5cdb7e98b76646fa4f7f46fcfefce47bba8ad5f43726ccb614d379b0c607815d118993f4aacb7b608fa833bda3e5cc0a89c19100fe973'
